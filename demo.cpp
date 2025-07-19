2a6e39ab521224b51b9ed38d12b50469c0a0ec47
Subodha <subodhaishere@gmail.com>
2025-07-19 15:33:58 +0530

    Update

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <ctime>
 
+void func(char board[]);
 
 int main(){
-  int *pointor;
-  
-  std::cout << *pointor;
-
+  char board[3] = {'w','e','f'};
+  func(board);
+  for(int i = 0 ; i <3; i ++){
+    std::cout << board[i]<<"\n";}
     return 0;
 }
 
+void func(char board[]){
+  board[0] = 'q';
+  for(int i = 0 ; i <3; i ++){
+    std::cout << board[i]<< "\n";
+  }
+}
